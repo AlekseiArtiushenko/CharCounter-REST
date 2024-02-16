371654168db5a4d727d81a319f923e61a9188806
Aleksei Artiushenko <aleksei.artiushenko.it@gmail.com>
2024-02-16 21:46:37 +0300

    Creating tests

--- src/test/java/ru/artiushenko/charcounterrest/LogicTests.java
+++ src/test/java/ru/artiushenko/charcounterrest/LogicTests.java
@@ -10,8 +10,8 @@ import java.util.Map;
 @SpringBootTest
 class LogicTests {
     @Test
-    void definingUniqueSymbols() {
-        String uniqueSymbolsString = "sГ2";
+    void definingRepeatedSymbols() {
+        String uniqueSymbolsString = "sГ22";
 
         UniqueSymbolsService uniqueSymbolsService = new UniqueSymbolsService();
         Map<Character, Integer> testMap = uniqueSymbolsService.parseUniqueSymbols(uniqueSymbolsString);
@@ -19,24 +19,7 @@ class LogicTests {
         Assertions.assertEquals(uniqueSymbolsString.length(), testMap.size(), "Размер результата должен быть равен длине строки с уникальными символами");
         Assertions.assertEquals(1, testMap.get('s'), "Количество символа 's' должно быть 1");
         Assertions.assertEquals(1, testMap.get('Г'), "Количество символа 'Г' должно быть 1");
-        Assertions.assertEquals(1, testMap.get('2'), "Количество символа '2' должно быть 1");
-    }
-
-    @Test
-    void definingRepeatedSymbols() {
-        String uniqueSymbolsString = "333221AAAББVv";
-
-        UniqueSymbolsService uniqueSymbolsService = new UniqueSymbolsService();
-        Map<Character, Integer> testMap = uniqueSymbolsService.parseUniqueSymbols(uniqueSymbolsString);
-
-        Assertions.assertEquals(7, testMap.size(), "Размер результата должен быть равен длине строки с уникальными символами");
-        Assertions.assertEquals(3, testMap.get('3'), "Количество символа '3' должно быть 3");
         Assertions.assertEquals(2, testMap.get('2'), "Количество символа '2' должно быть 2");
-        Assertions.assertEquals(1, testMap.get('1'), "Количество символа '1' должно быть 1");
-        Assertions.assertEquals(3, testMap.get('A'), "Количество символа 'A' должно быть 3");
-        Assertions.assertEquals(2, testMap.get('Б'), "Количество символа 'Б' должно быть 2");
-        Assertions.assertEquals(1, testMap.get('V'), "Количество символа 'V' должно быть 1");
-        Assertions.assertEquals(1, testMap.get('v'), "Количество символа 'v' должно быть 1");
     }
 
     @Test
